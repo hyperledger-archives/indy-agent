389b054dc31db59d6e3d5b44b4b6b626301d0b13
Michael Boyd <michaeldavisboyd@gmail.com>
2018-05-14 17:09:35 -0600

    added basic sandbox on encrypting messages
    Signed-off-by: Michael Boyd <michaeldavisboyd@gmail.com>

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(
+    name='python3-indy-agent',
+    version='0.1',
+    url='https://github.com/hyperledger/indy-agent',
+    license='MIT/Apache-2.0',
+    description='Reference Agent for the Indy-SDK',
+    install_requires=['python3-indy']
+)
