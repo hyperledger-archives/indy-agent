82e26cc6ce964af098df429e1ee4119eb58186ef
Daniel Bluhm <bluhmdan@gmail.com>
2018-05-16 16:27:23 -0600

    Init run before everything else
    Signed-off-by: Daniel Bluhm <bluhmdan@gmail.com>

--- python/indy-agent.py
+++ python/indy-agent.py
@@ -25,7 +25,7 @@ wallet_handle = None
 me = None
 
 async def init():
-    me = input('Who are you? ').strip()
+    _me = input('Who are you? ').strip()
     wallet_name = '%s-wallet' % me
 
     #Create Wallet and Get Wallet Handle
@@ -35,11 +35,11 @@ async def init():
     except:
         pass
 
-    wallet_handle = await wallet.open_wallet(wallet_name, None, None)
-    print('wallet = %s' % wallet_handle)
+    _wallet_handle = await wallet.open_wallet(wallet_name, None, None)
+
+    return _wallet_handle, _me
 
 async def main():
-    await init()
 
     await router.register("CONN_REQ", connection.handle_request)
     await router.register("CONN_RES", connection.handle_response)
@@ -47,9 +47,7 @@ async def main():
     while True:
         print('would you like to send a connection request? [Y/n]')
         msg_bytes = await receiver.recv()
-        print("Message received:\n\tbytes: {}".format(msg_bytes))
         msg = Serializer.unpack(msg_bytes)
-        print("\tType: {}, Data: {}".format(msg.type, msg.data))
         await router.route(msg, wallet_handle)
 
 def cli():
@@ -58,6 +56,8 @@ def cli():
         loop.create_task(connection.send_request(1, 'bob'))
 
 try:
+    wallet_handle, me = loop.run_until_complete(init())
+    print('wallet = {}, me = {}'.format(wallet_handle, me))
     loop.add_reader(sys.stdin, cli)
     loop.create_task(receiver.start(loop))
     loop.create_task(main())
