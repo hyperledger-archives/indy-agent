54ab82b0ec58f982679bf396eaa5e53945e48493
Michael Boyd <michaeldavisboyd@gmail.com>
2018-05-17 16:39:59 -0600

    added jinja template engine
    Signed-off-by: Michael Boyd <michaeldavisboyd@gmail.com>

--- python/indy-agent.py
+++ python/indy-agent.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 import sys
+import socket
 from aiohttp import web
 from indy import did, wallet, pool
 
@@ -9,7 +10,10 @@ from receiver.aiohttp_receiver import AioHttpReceiver as Receiver
 from router.simple_router import SimpleRouter as Router
 import modules.connection as connection
 import serializer.json_serializer as Serializer
+
+import jinja2
 from aiohttp_index import IndexMiddleware
+import aiohttp_jinja2
 
 if 'INDY_AGENT_PORT' in os.environ.keys():
     port = int(os.environ['INDY_AGENT_PORT'])
@@ -22,8 +26,12 @@ agent['msg_router'] = Router()
 agent['msg_receiver'] =  Receiver(asyncio.Queue())
 agent['initialized'] = False
 
+# template engine setup
+aiohttp_jinja2.setup(agent,
+    loader=jinja2.FileSystemLoader(os.path.realpath('view/templates/')))
+
 routes = [
-    web.static('/', os.path.realpath('view/')),
+    web.get('/', view.index),
     web.post('/indy', agent['msg_receiver'].handle_message),
     web.post('/indy/init', view.init)
 ]
@@ -41,7 +49,7 @@ async def main(agent):
 
     await msg_router.register("CONN_REQ", connection.handle_request)
     await msg_router.register("CONN_RES", connection.handle_response)
-
+    print('===== Server Started on: http://localhost:{} ====='.format(port))
     while True:
         msg_bytes = await msg_receiver.recv()
         msg = Serializer.unpack(msg_bytes)
