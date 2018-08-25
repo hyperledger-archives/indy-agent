fdde22b8402052527afcef780a6b156cb2ffd98c
Kuzma Leshakov <k.leshakov@yandex.ru>
2018-08-25 20:08:47 +0300

    Adapt agent to Indy 1.6 Signed-off-by: Kuzma Leshakov <k.leshakov@yandex.ru>

--- python/modules/init.py
+++ python/modules/init.py
@@ -4,8 +4,11 @@
 # pylint: disable=import-error
 
 import json
+
 from indy import wallet, did
+
 import modules.ui as ui
+
 
 async def initialize_agent(msg, agent):
     """ Initialize agent.
@@ -13,23 +16,27 @@ async def initialize_agent(msg, agent):
     data = msg.message
     agent.owner = data['name']
     passphrase = data['passphrase']
+    wallet_config = json.dumps({"id": "wallet"})
+    wallet_credentials = json.dumps({"key": "wallet_key"})
 
     wallet_name = '%s-wallet' % agent.owner
 
     # pylint: disable=bare-except
     # TODO: better handle potential exceptions.
     try:
-        await wallet.create_wallet('pool1', wallet_name, None, None, json.dumps({"key": passphrase}))
+        await wallet.create_wallet(wallet_config, wallet_credentials)
     except Exception as e:
         print(e)
 
     try:
-        agent.wallet_handle = await wallet.open_wallet(wallet_name, None, json.dumps({"key": passphrase}))
+        agent.wallet_handle = await wallet.open_wallet(wallet_config,
+                                                       wallet_credentials)
     except Exception as e:
         print(e)
         print("Could not open wallet!")
 
-    (_, agent.endpoint_vk) = await did.create_and_store_my_did(agent.wallet_handle, "{}")
+    (_, agent.endpoint_vk) = await did.create_and_store_my_did(
+        agent.wallet_handle, "{}")
 
     agent.initialized = True
     return await ui.ui_connect(None, agent)
