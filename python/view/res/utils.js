b22b0e40a48f95525b276748b99d713a86dfed30
Kuzma Leshakov <k.leshakov@yandex.ru>
2018-08-25 20:16:56 +0300

    [IA-11] Add todayDate method Signed-off-by: Kuzma Leshakov <k.leshakov@yandex.ru>

--- python/view/res/utils.js
+++ python/view/res/utils.js
@@ -3,3 +3,18 @@ function removeElementById(id) {
     var element = document.getElementById(id);
     element.parentNode.removeChild(element);
 }
+
+function getTodayDate() {
+    var tdate = new Date();
+    var dd = tdate.getDate(); //yields day
+    var MM = tdate.getMonth(); //yields month
+    var yyyy = tdate.getFullYear(); //yields year
+
+    var s = tdate.getSeconds();
+    var m = tdate.getMinutes();
+    var h = tdate.getHours();
+
+    var currentDate = dd + "-" + (MM + 1) + "-" + yyyy + " " + h + ":" + m + ":" + s;
+
+    return currentDate;
+}
