7c390ff1a4e5a5b11e03d242b099f64628a2d508
Daniel Bluhm <daniel.bluhm@sovrin.org>
2018-10-15 14:52:45 -0600

    Revert "Implement connection protocol in python agent (#25)"
    This reverts commit a36ff708420e8b6e8b4c2a48d5ef8ee351b2d567.
    
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

--- python/receiver/message_receiver.py
+++ python/receiver/message_receiver.py
@@ -2,20 +2,16 @@
 
 # pylint: disable=import-error
 from aiohttp import web
-import asyncio
 
 class MessageReceiver():
     """ Simple message queue interface for receiving messages.
     """
-    def __init__(self):
-        self.msg_queue = asyncio.Queue()
+    def __init__(self, queue):
+        self.msg_queue = queue
 
     async def handle_message(self, request):
         """ Put to message queue and return 202 to client.
         """
-        if not request.app['agent'].initialized:
-            raise web.HTTPUnauthorized()
-
         msg = await request.read()
         await self.msg_queue.put(msg)
         raise web.HTTPAccepted()
--- python/serializer/json_serializer.py
+++ python/serializer/json_serializer.py
@@ -1,35 +1,16 @@
-"""
-Serializer using json as i/o format.
+""" Serializer using json as i/o format.
 """
 
 import json
-
 from model import Message
 
-
-def unpack_dict(dictionary: dict) -> Message:
-    deserialized_msg = Message(**dictionary)
-    return deserialized_msg
-
-
-def unpack(dump) -> Message:
+def unpack(dump: bytes) -> Message:
+    """ Deserialize from json string to Message.
     """
-    Deserialize from bytes or str to Message
+    msg_dict = json.loads(dump)
+    return Message(msg_dict['type'], msg_dict['did'], msg_dict['data'])
+
+def pack(msg: Message) -> bytes:
+    """ Serialize from Message to json string.
     """
-    dump_dict = json.loads(dump)
-    deserialized_msg = Message(**dump_dict)
-    return deserialized_msg
-
-
-def pack(msg: Message) -> str:
-    """
-    Serialize from Message to json string or from dictionary to json string.
-    """
-
-    class MessageEncoder(json.JSONEncoder):
-        def default(self, obj):
-            if isinstance(obj, Message):
-                return obj.to_dict()
-            return json.JSONEncoder.default(self, obj)
-
-    return json.dumps(msg, cls=MessageEncoder)
+    return json.dumps({"type": msg.msg_type, "did": msg.did, "data": msg.data})
