bbfb8beff4f9c453afc2ef4e31676fad8589f84e
Daniel Bluhm <daniel.bluhm@sovrin.org>
2019-01-22 16:30:37 -0700

    Connection test, testing agent
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

--- test-suite/tests/__init__.py
+++ test-suite/tests/__init__.py
@@ -1,10 +1,15 @@
 """ Module containing Agent Test Suite Tests.
 """
+import base64
+import json
 import asyncio
 import pytest
 from pytest import fail
 from typing import Callable
+from indy import crypto
 
+from message import Message
+from serializer import JSONSerializer as Serializer
 from transport import BaseTransport
 
 async def expect_message(transport: BaseTransport, timeout: int):
@@ -25,8 +30,42 @@ async def expect_message(transport: BaseTransport, timeout: int):
 
     fail("No message received before timing out")
 
-def validate_message(expected_attrs, msg):
+def validate_message(expected_attrs: [str], msg: Message):
     __tracebackhide__ = True
     for attribute in expected_attrs:
         if attribute not in msg:
             fail("Attribute \"{}\" is missing from msg:\n{}".format(attribute, msg))
+
+async def pack(wallet_handle: int, my_vk: str, their_vk: str, msg: Message) -> str:
+    return json.dumps({
+        'to': their_vk,
+        'from': my_vk,
+        'payload': base64.b64encode(
+            await crypto.auth_crypt(
+                wallet_handle,
+                my_vk,
+                their_vk,
+                bytes(Serializer.pack(msg), 'utf-8')
+            )
+        ).decode('ascii')
+    })
+
+async def unpack(wallet_handle: int, wire_msg_bytes: bytes, **kwargs) -> Message:
+    __tracebackhide__ = True
+    wire_msg = json.loads(wire_msg_bytes)
+
+    if 'expected_to_vk' in kwargs:
+        assert kwargs['expected_to_vk'] == wire_msg['to'], 'Message is not for the expected verkey!'
+
+    if 'expected_from_vk' in kwargs:
+        assert kwargs['expected_from_vk'] == wire_msg['from'], 'Message is not from the expected verkey!'
+
+    their_vk, their_data = await crypto.auth_decrypt(
+        wallet_handle,
+        wire_msg['to'],
+        base64.b64decode(wire_msg['payload'])
+    )
+
+    assert their_vk == wire_msg['from'], 'Message is not from reported key!'
+
+    return Serializer.unpack(their_data.decode('utf-8'))
--- test-suite/tests/connection/test_connection.py
+++ test-suite/tests/connection/test_connection.py
@@ -4,11 +4,21 @@ import re
 import base64
 from message import Message
 from serializer import JSONSerializer as Serializer
-from tests import expect_message, validate_message
+from tests import expect_message, validate_message, pack, unpack
 from indy import did
 
+class Connection:
+
+    FAMILY_NAME = "connections"
+    VERSION = "1.0"
+    FAMILY = "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/" + FAMILY_NAME + "/" + VERSION + "/"
+
+    INVITE = FAMILY + "invite"
+    REQUEST = FAMILY + "request"
+    RESPONSE = FAMILY + "response"
+
 @pytest.mark.asyncio
-async def test_connection_started_by_tested_agent(config, transport):
+async def test_connection_started_by_tested_agent(config, wallet_handle, transport):
     invite_url = input('Input generated connection invite: ')
 
     matches = re.match('(.+)?c_i=(.+)', invite_url)
@@ -26,4 +36,43 @@ async def test_connection_started_by_tested_agent(config, transport):
             'endpoint'
         ],
         invite_msg
+    )
+
+    # Create my information for connection
+    (my_did, my_vk) = await did.create_and_store_my_did(wallet_handle, '{}')
+
+
+    # Send Connection Request to inviter
+    request = Message({
+        '@type': Connection.REQUEST,
+        'label': 'testing-agent',
+        'DID': my_did,
+        'DIDDoc': {
+            'key': my_vk,
+            'endpoint': config.endpoint,
+        }
+    })
+
+    await transport.send(invite_msg['endpoint'], await pack(wallet_handle, my_vk, invite_msg['key'], request))
+
+    # Wait for response
+    response_bytes = await expect_message(transport, 60)
+
+    response = await unpack(wallet_handle, response_bytes, expected_to_vk=my_vk)
+
+    validate_message(
+        [
+            '@type',
+            'DID',
+            'DIDDoc'
+        ],
+        response
+    )
+
+    validate_message(
+        [
+            'key',
+            'endpoint'
+        ],
+        response['DIDDoc']
     )
