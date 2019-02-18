681c7c5f474ce848865565a45fc18eb797b8fef9
Daniel Bluhm <daniel.bluhm@sovrin.org>
2019-02-18 15:24:31 -0700

    Add tests for trustping
    Minor fixes discovered while testing trustping protocol
    
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

---
+++ test-suite/tests/trustping/__init__.py
@@ -0,0 +1,54 @@
+import uuid
+from tests import validate_message
+from message import Message
+
+class TrustPing():
+    FAMILY_NAME = "trustping"
+    VERSION = "1.0"
+    FAMILY = "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/" + FAMILY_NAME + "/" + VERSION + "/"
+
+    PING = FAMILY + "ping"
+    PING_REPONSE = FAMILY + "ping_response"
+
+    class Ping():
+        @staticmethod
+        def build():
+            return Message({
+                '@type': TrustPing.PING,
+                '@id': str(uuid.uuid4())
+            })
+
+        @staticmethod
+        def validate(message):
+            validate_message(
+                [
+                    ('@type', TrustPing.PING),
+                    '@id'
+                ],
+                message
+            )
+
+    class Pong():
+        @staticmethod
+        def build(ping_id: str):
+            return Message({
+                '@type': TrustPing.PING_REPONSE,
+                '~thread': {'thid': ping_id }
+            })
+
+        @staticmethod
+        def validate(message, ping_id):
+            validate_message(
+                [
+                    ('@type', TrustPing.PING_REPONSE),
+                    '~thread'
+                ],
+                message
+            )
+
+            validate_message(
+                [
+                    ('thid', ping_id)
+                ],
+                message['~thread']
+            )
